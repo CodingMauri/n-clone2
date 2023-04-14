b1e997c24a47c7fd56af1f7d2affcd7932779823
Mauricio Alvarenga <mauricioalvarenga@Mauricios-MacBook-Pro.local>
2023-04-13 18:34:49 -0700

    added slider functionality for the carousels going to move on to setting up the backend part of the application

--- src/Components/MovieCarousel.jsx
+++ src/Components/MovieCarousel.jsx
@@ -1,12 +1,11 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { FaHeart, FaRegHeart } from "react-icons/fa";
-
-const MovieCarousel = ({ movies, genre }) => {
+import Movie from "./Movie";
+import {MdChevronLeft,MdChevronRight} from "react-icons/md"
+const MovieCarousel = ({ movies, genre ,rowId}) => {
   //calling for genres
 
   const [genreName, setGenreName] = useState("");
-  const [like, setLike] = useState(false);
   // console.log(genre)
   const getGenres = () => {
     axios
@@ -26,38 +25,32 @@ const MovieCarousel = ({ movies, genre }) => {
     getGenres();
   }, [genre]);
 
-  const imagePath = "https://image.tmdb.org/t/p/w500/";
+  const slideLeft = () => {
+    let slider = document.getElementById("slider" + rowId)
+    slider.scrollLeft = slider.scrollLeft - 500
+  }
+  const slideRight = () => {
+    let slider = document.getElementById("slider" + rowId)
+    slider.scrollLeft = slider.scrollLeft + 500
+  }
+
   return (
     <>
       <h1 className="text-white font-bold md:text-xl p-4">
         {genreName} movies
       </h1>
-      <div className="relative flex items-center">
-        <div id={"slider"}>
+      <div className="relative flex items-center group:">
+        <MdChevronLeft onClick = {slideLeft} className = "bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 group-hover:block" size={40} />
+        <div
+          id={"slider" + rowId}
+          className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative"
+        >
           {movies.map((item, id) => {
-            return (
-              <div className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2">
-                <img
-                  className="w-full h-auto block"
-                  src={`https://image.tmdb.org/t/p/w500/${item?.poster_path}`}
-                  alt={item?.title}
-                />
-                <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white">
-                  <p className="white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">
-                    {item?.title}
-                  </p>
-                  <p>
-                    {like ? (
-                      <FaHeart className="absolute top-4 left-4 text-gray-300" />
-                    ) : (
-                      <FaRegHeart className="absolute top-4 left-4 text-gray-300" />
-                    )}
-                  </p>
-                </div>
-              </div>
-            );
+            return <Movie item={item} id={id} />;
           })}
         </div>
+        <MdChevronRight onClick = {slideRight} className = "bg-white rounded-full absolute opacity-50 hover:opacity-100  z-20  cursor:pointer group-hover:block right-0" size={40} />
+
       </div>
     </>
   );
