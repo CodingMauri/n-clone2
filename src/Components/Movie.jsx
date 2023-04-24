7b89dd18c0783992e14bc3bb5edfa5281ec7f630
Mauricio Alvarenga <mauricioalvarenga@Mauricios-MacBook-Pro.local>
2023-04-23 19:37:52 -0700

    Finished the trailer css now im trying to get the showTrailer state up to a higher component

--- src/Components/Movie.jsx
+++ src/Components/Movie.jsx
@@ -1,34 +1,29 @@
-import {React,useState} from 'react'
-import { FaHeart, FaRegHeart } from "react-icons/fa";
-import {motion} from "framer-motion"
-import ExpandedMovie from "./ExpandedMovie"
-
-function Movie({item}) {
-    const [like, setLike] = useState(false);
-
-    const [expand,setExpand] = useState(null)
-
+import { React, useState } from "react";
+import ExpandedMovie from "./ExpandedMovie";
+function Movie({ item ,setShowTrailer}) {
+  const [expand, setExpand] = useState(null);
   return (
-    <div whileHover={{transition:1 ,scale:0.9}}
-      className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2 "
-      onMouseEnter = {() => setExpand(item)}
-      onMouseLeave = {() => setExpand(null)}>
-      {expand === item && <ExpandedMovie item = {item} />}
-    <img
-      className="w-full h-auto block"
-      src={`https://image.tmdb.org/t/p/w500/${item?.poster_path}`}
-      alt={item?.title}
-      id = {item.id}
-      />
+    
+    <div
+    whileHover={{ transition: 1, scale: 0.9 }}
+    className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2 "
+    onMouseEnter={() => setExpand(item)}
+    onMouseLeave={() => setExpand(null)}
+    >
+        {expand === item && (
+          <ExpandedMovie item={item} setShowTrailer={setShowTrailer} />
+          )}
+        <img
+          className="w-full h-auto block"
+          src={`https://image.tmdb.org/t/p/w500/${item?.poster_path}`}
+          alt={item?.title}
+          id={item.id}
+        />
 
-    <div className="absolute bottom-0 w-full opacity-0 hover:opacity-100 text-white">
-      
-      
-    </div>
-
-   
-  </div>
-  )
+        <div className="absolute bottom-0 w-full opacity-0 hover:opacity-100 text-white"></div>
+      </div>
+    
+  );
 }
 
-export default Movie
+export default Movie;
--- src/Components/Trailer.jsx
+++ src/Components/Trailer.jsx
@@ -1,25 +1,52 @@
 import React from "react";
+import {useState,useEffect} from "react"
 import Youtube from "react-youtube";
+import { getYoutubeData } from "../Requests";
+function Trailer({movieId}) {
+  const [selected,setSelected] = useState([])
+  useEffect(() => {
+    getYoutubeData(movieId, setSelected);
+  }, [movieId]);
+    
+  console.log(movieId);
+  const endTrailer = (e) => {
+    if (e.data === window.YT.PlayerState.ENDED) {
+      e.target.pauseVideo();
+    }
+  };
 
-function Trailer({officialTrailer,youtubePlayer,endTrailer,fallbackTrailer}) {
+  const officialTrailer = selected
+    ?.filter((video) => video.type === "Trailer" && video.official)
+    .find((video) => video.site === "YouTube");
 
-    const opts = {
-        height: `80%`,
-        width: `80%`,
-        playerVars: {
-          autoplay: 1,
-        },
-      };
-  return <>
-     {officialTrailer && youtubePlayer && (
-            <Youtube
-              className = "absolute z-70"
-              videoId={officialTrailer?.key || fallbackTrailer?.key}
-              opts={opts}
-              onStateChange={endTrailer}
-            />
-          )}
-  </>;
+  const fallbackTrailer = selected
+    ?.filter((video) => video.type === "Trailer")
+    .find((video) => video.site === "YouTube");
+
+ 
+
+  const opts = {
+    height: "800",
+    width: "800",
+
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+  return (
+    <>
+      {officialTrailer && (
+        <div className="w-[90%] h-[90%] relative z-90">
+          <Youtube
+            className="absolute z-70"
+            videoId={officialTrailer?.key || fallbackTrailer?.key}
+            opts={opts}
+            onStateChange={endTrailer}
+          />
+        </div>
+      )}
+    </>
+  );
 }
 
 export default Trailer;
