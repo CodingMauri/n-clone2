8a179a149e988e8406eb4a490f8e2d951fddac76
CodingMauri <106369465+CodingMauri@users.noreply.github.com>
2023-05-10 16:57:06 -0700

    Adjusting for deployment

--- src/Components/ExpandedMovie.jsx
+++ src/Components/ExpandedMovie.jsx
@@ -1,4 +1,4 @@
-import { React , useState} from "react";
+import { React} from "react";
 import { FiPlayCircle } from "react-icons/fi";
 import { motion } from "framer-motion";
 import { useNavigate } from "react-router-dom";
